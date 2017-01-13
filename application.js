b90476b4abbd19aea41960752b59df3d45de6267
Aiden Keating <akeating@redhat.com>
2017-01-13 12:26:52 +0000

    Add changes for Sync Horizontal Scaling testing

--- application.js
+++ application.js
@@ -1,29 +1,46 @@
+var cluster = require('cluster');
 var express = require('express');
-var mbaasApi = require('fh-mbaas-api');
+var bodyParser = require('body-parser');
+var mbaasApi = require('fh-mbaas-api-metrics');
 var mbaasExpress = mbaasApi.mbaasExpress();
+var cpuCount = require('os').cpus().length;
 
-// Define custom sync handlers and interceptors
-require('./lib/sync.js');
+if (cluster.isMaster && process.env.SHOULD_SCALE === 'true') {
+  // Scale to defined number of workers, or just the amount of cores
+  var clusterSize = process.env.WORKER_COUNT || cpuCount;
 
-// Securable endpoints: list the endpoints which you want to make securable here
-var securableEndpoints = [];
+  for(var i = 0; i < clusterSize; i++) {
+    console.log('Starting worker ' + (i + 1) + ' with CPU count of ' + cpuCount);
+    cluster.fork(Object.assign({}, process.env, { metricsId: 'worker-' + (i + 1) }));
+  }
+} else {
 
-var app = express();
+  // Define custom sync handlers and interceptors
+  require('./lib/sync.js');
 
-// Note: the order which we add middleware to Express here is important!
-app.use('/sys', mbaasExpress.sys(securableEndpoints));
-app.use('/mbaas', mbaasExpress.mbaas);
+  // Securable endpoints: list the endpoints which you want to make securable here
+  var securableEndpoints = [];
 
-// Note: important that this is added just before your own Routes
-app.use(mbaasExpress.fhmiddleware());
+  var app = express();
 
-// Add extra routes here
+  // Note: the order which we add middleware to Express here is important!
+  app.use('/sys', mbaasExpress.sys(securableEndpoints));
+  app.use('/mbaas', mbaasExpress.mbaas);
 
-// Important that this is last!
-app.use(mbaasExpress.errorHandler());
+  // Note: important that this is added just before your own Routes
+  app.use(mbaasExpress.fhmiddleware());
 
-var port = process.env.FH_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8001;
-var host = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-var server = app.listen(port, host, function() {
-  console.log("App started at: " + new Date() + " on port: " + port); 
-});
+  // Add extra routes here
+  app.post('/collection/reset', bodyParser.json({limit: '1mb'}), require('./lib/collection').reset);
+  app.post('/collection/seed', bodyParser.json({limit: '100mb'}), require('./lib/collection').seed);
+  app.post('/sync/all', bodyParser.json({ limit: '100mb' }), require('./lib/test').allSync);
+
+  // Important that this is last!
+  app.use(mbaasExpress.errorHandler());
+
+  var port = process.env.FH_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8001;
+  var host = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+  var server = app.listen(port, host, function() {
+    console.log("App started at: " + new Date() + " on port: " + port); 
+  });
+}
